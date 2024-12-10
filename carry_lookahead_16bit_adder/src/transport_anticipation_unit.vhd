23d8ade11b6aabab6252c73db80bfc2508108e48
matei <mateibalan04@gmail.com>
2024-12-10 13:31:37 +0200

    modified TUA 16 bit, implemented carry output with buffer

--- carry_lookahead_16bit_adder/src/transport_anticipation_unit.vhd
+++ carry_lookahead_16bit_adder/src/transport_anticipation_unit.vhd
@@ -9,13 +9,11 @@ entity TAU is
         Cin     : in  STD_LOGIC;                    -- Carry-in
         P_group : out STD_LOGIC;                   -- Group propagate
         G_group : out STD_LOGIC;                   -- Group generate
-        C_out   : out STD_LOGIC_VECTOR(3 downto 0) -- Carry-out signals
+        C_out   : buffer STD_LOGIC_VECTOR(3 downto 0) -- Carry-out signals
     );
 end TAU;
 
 architecture Behavioral of TAU is
-    -- Internal Signals
-    signal C_out_vector : STD_LOGIC_VECTOR(3 downto 0);
 begin
     -- Group Propagate
     P_group <= P(0) and P(1) and P(2) and P(3);
@@ -23,12 +21,9 @@ begin
     -- Group Generate
     G_group <= G(3) or (P(3) and G(2)) or (P(3) and P(2) and G(1)) or (P(3) and P(2) and P(1) and G(0));
 
-    -- Compute carry-out internal signals
-    C_out_vector(0) <= G(0) or (P(0) and Cin);
-    C_out_vector(1) <= G(1) or (P(1) and C_out_vector(0));
-    C_out_vector(2) <= G(2) or (P(2) and C_out_vector(1));
-    C_out_vector(3) <= G(3) or (P(3) and C_out_vector(2));
-
-    -- Final Carry-Out
-    C_out <= C_out_vector;
+    -- Compute carry-out
+    C_out(0) <= G(0) or (P(0) and Cin);
+    C_out(1) <= G(1) or (P(1) and C_out(0));
+    C_out(2) <= G(2) or (P(2) and C_out(1));
+    C_out(3) <= G(3) or (P(3) and C_out(2));
 end Behavioral;
